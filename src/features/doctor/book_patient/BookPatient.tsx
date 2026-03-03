fea9611674c4682b8ee5eec943ad40f1f246f40c
Jerome Obogne <renfah29@gmail.com>
2026-03-03 20:49:29 +0800

    features(Book Appoinment): remove unnecessary comments

--- src/features/doctor/book_patient/BookPatient.tsx
+++ src/features/doctor/book_patient/BookPatient.tsx
@@ -3,25 +3,12 @@ import { AppointmentForm } from './Components';
 
 
 const BookPatient = () => { 
-    // const [date,setDate] = useState<Dayjs |null>(null)
-    // const handleChange = (newValue: Dayjs | null) => {
-    //   setDate(newValue);
-    // };
-
+  
   return (
     <>
       <div>
         <AppointmentForm/>
-        {/* <DateCalendar
-          slots={{
-            day: CustomDay,
-          }}
-          slotProps={{
-            day: { dateRecords: highlightedDates } as any,
-          }}
-          onChange={handleChange}
-          value={date}
-        /> */}
+      
       </div>
     </>
   );
